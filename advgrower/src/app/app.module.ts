59c8d8176c98cdadaa00679a5e1c7d3cc1accb1d
Poonam Budhekar <poonamb@bitsdev.in>
2019-09-11 11:37:49 +0530

    header/footer

--- advgrower/src/app/app.module.ts
+++ advgrower/src/app/app.module.ts
@@ -8,13 +8,17 @@ import { MaterialModule } from './shared/material.module'
 import { FormsModule } from '@angular/forms';
 import { AboutComponent } from './shared/components/about/about.component';
 import { ContactComponent } from './shared/components/contact/contact.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
